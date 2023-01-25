a1a317d3e69b6712b8bb5ac394a7c54422ea6ce1
¨Tomas <tomasgabriel1999@hotmail.com>
2023-01-25 19:46:57 -0300

    agregamos informacion acerca de mi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Sobremi from "./components/SobreMi";
 
 function App() {
   return (
  <>
  <Navbar/>
  <Header/>
+ <Sobremi/>
  <Footer/>
  </>
  
---
+++ src/components/SobreMi.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {aside} from "react-bootstrap";
+
+const Sobremi = () => {
+  return ( 
+    <div>
+    <h2 className="lead">Hola! Soy Tomás</h2>
+    <h4 className="text-danger display-4 text-dark">Programador FullStack Web Developer.</h4>
+    <p class="text-dark lead">
+                    Aventurero, deportista y curioso. Actualmente egresado de Rolling Code School. Me gusta siempre estar aprendiendo un poco mas! Super responsable y con todas las ganas de trabajar!!!
+                  </p>
+                  <aside class="col-12 col-md-6 acercademi">
+                
+                </aside>
+    </div>
+   
+
+   );
+}
+ 
+export default Sobremi;

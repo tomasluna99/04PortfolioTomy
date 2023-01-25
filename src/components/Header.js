6a38211d2d3cc2af3d468248225b1b11f574a450
¨Tomas <tomasgabriel1999@hotmail.com>
2023-01-25 13:39:39 -0300

    agregamos estilos y una imagen de portada

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return ( 
+    <div className="header-wraper">
+      <div className="main-info">
+      <h1>Bienvenidos</h1>
+      </div>
+    </div>
+   );
+}
+ 
+export default Header;

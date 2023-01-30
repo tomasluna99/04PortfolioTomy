3a48c22eff5a94a1bd031f241ee7a0d521be60b8
¨Tomas <tomasgabriel1999@hotmail.com>
2023-01-30 15:42:50 -0300

    modificamos el navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,54 @@
 import React from "react";
 
 const Navbar = () => {
-  return ( 
-    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark">
-  <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active text-white" aria-current="page" href="#">Inicio</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white" href="#">Proyectos</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white" href="#">Sobre mi</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-white" href="#">Contacto</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-   );
-}
- 
+  return (
+    <>
+    
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Bienvenidos
+          </a>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            aria-label="Toggle navigation"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            data-mdb-target="#navbarSupportedContent"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        
+
+              </ul>
+
+            <ul className="navbar-nav d-flex flex-row me-1">
+              <li className="nav-item me-3 me-lg-0">
+                <a className="nav-link text-white" href="https://www.facebook.com/profile.php?id=100007230225536">
+                  <i className="bi bi-facebook"></i>
+                </a>
+              </li>
+              <li className="nav-item me-3 me-lg-0">
+                <a className="nav-link text-white" href="https://www.instagram.com/tomas.luna99/">
+                  <i className="bi bi-instagram"></i>
+                </a>
+              </li>
+              <li className="nav-item me-2">
+                <a className="nav-link text-white" href="https://github.com/tomasluna99?tab=repositories"><i class="bi bi-github"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+     
+    </>
+  );
+};
+
 export default Navbar;

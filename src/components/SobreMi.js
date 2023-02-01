5e43c216645cac62cb05e350eb6962c0b26ce915
¨Tomas <tomasgabriel1999@hotmail.com>
2023-02-01 17:19:57 -0300

    agregamos estilos a nuestras herramientas

--- src/components/SobreMi.js
+++ src/components/SobreMi.js
@@ -15,17 +15,18 @@ const Sobremi = () => {
         </p>
           <img src={Banner1} className="banner" />
       </section>
-      <section class="container my-5">
-        <article class="row text-center centrarCards">
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0 margen">
-            <div class="card">
+      <section className="container my-5">
+        <h2 className="text-center display-4">Mis herramientas</h2>
+        <article className="row text-center centrarCards">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0 margen">
+            <div>
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="js"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   Javascript
                   
                 </h5>
@@ -33,15 +34,15 @@ const Sobremi = () => {
               </div>
             </div>
           </aside>
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0">
-            <div class="card margen">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0">
+            <div>
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="Css"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   CSS
                   
                 </h5>
@@ -49,15 +50,15 @@ const Sobremi = () => {
               </div>
             </div>
           </aside>
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0">
-            <div class="card margen">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0">
+            <div >
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="bootstrap"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   Bootstrap
                   
                 </h5>
@@ -65,45 +66,45 @@ const Sobremi = () => {
               </div>
             </div>
           </aside>
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0">
-            <div class="card margen">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0">
+            <div>
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="Javascript"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   Javascript
                 </h5>
                 
               </div>
             </div>
           </aside>
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0">
-            <div class="card margen">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0">
+            <div>
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="React"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   React
                 </h5>
                
               </div>
             </div>
           </aside>
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0">
-            <div class="card margen">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0">
+            <div>
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="nodejs"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   Node-Js
                   
                 </h5>
@@ -111,15 +112,15 @@ const Sobremi = () => {
               </div>
             </div>
           </aside>
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0">
-            <div class="card margen">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0">
+            <div>
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="mongodb"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   Mongo DB
                   
                 </h5>
@@ -127,15 +128,15 @@ const Sobremi = () => {
               </div>
             </div>
           </aside>
-          <aside class="col-10 col-md-4 col-lg-3 m-0 p-0">
-            <div class="card margen">
+          <aside className="col-10 col-md-4 col-lg-3 m-0 p-0">
+            <div>
               <img
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                class="card-img-top"
+                className="card-img-top logos"
                 alt="Github"
               />
-              <div class="card-body">
-                <h5 class="card-title">
+              <div className="card-body">
+                <h5 className="card-title">
                   Github
                   
                 </h5>

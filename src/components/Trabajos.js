067c263605fc3fe9e2c0e22e06c9bf1787ed4875
¨Tomas <tomasgabriel1999@hotmail.com>
2023-02-21 12:25:19 -0300

    agregamos cards para nuestros trabajos

--- src/components/Trabajos.js
+++ src/components/Trabajos.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Bootstrap from "react-bootstrap";
-import Banner3 from "../assets/img/tomy3.png"
 
 const Trabajos = () => {
   return ( 
@@ -11,15 +10,134 @@ const Trabajos = () => {
       <h3 className="display-5 text-center">Mis Trabajos</h3>
 
       <p className="lead text-center">A continucacion les dejo unos trabajos míos realizados durante el curso y usos propios. Realizando ejercicios con Js, Bootstrap, un trabajo grupal aprendiendo el uso de Git, implementando el uso de React y muchas practicas mas!
+      
       </p>
       
-      <img src={Banner3} className="tomy-img" />
 
 
     </div>
 
 
     </section>
+
+     <section>
+<article class="row">
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna <span class="badge badge-pill text-bg-warning">Nuevo</span></h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Ver más
+</button> 
+    </div>
+  </div>
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Tarta de chocolate</h5> <span class="badge rounded-pill text-bg-info">Nuevo</span>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="detalleproducto.html" class="btn btn-primary">Ver mas</a>
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary disabled" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="" class="card-img-top" alt="Medialuna" />
+    <div class="card-body">
+      <h5 class="card-title">Medialuna</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+</article>
+
+ 
+</section>
     
     
     

d18962e5a7e1697883b017e67f517e3bb1c5708a
¨Tomas <tomasgabriel1999@hotmail.com>
2023-02-24 17:34:52 -0300

    agregamos card de proyectos trabajados

--- src/components/Trabajos.js
+++ src/components/Trabajos.js
@@ -20,50 +20,36 @@ const Trabajos = () => {
 
     </section>
 
-     <section>
+    <section>
 <article class="row">
 <aside class="col-12 col-md-4 col-lg-3">
   <div class="card">
-    <img src="" class="card-img-top" alt="Medialuna" />
+    <img src="https://cocudelicias.netlify.app/img/slider_201603_lemonpie.jpg" class="card-img-top" alt="Pasteleria" />
     <div class="card-body">
-      <h5 class="card-title">Medialuna <span class="badge badge-pill text-bg-warning">Nuevo</span></h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Ver más
-</button> 
+      <h5 class="card-title">Cocu Delicias</h5>
+      <p class="card-text">Trabajo realizado mediante el cursado donde aplicamos el uso de HTML, Bootstrap y practicas con Media Query para luego subir el proyecto a Netlify.</p>
+      <a href="https://cocudelicias.netlify.app/" target="_blank" className="btn btn-primary">Ver mas</a>
     </div>
   </div>
 </aside>
 <aside class="col-12 col-md-4 col-lg-3">
   <div class="card">
-    <img src="" class="card-img-top" alt="Medialuna" />
+    <img src="https://rollingbank-e.netlify.app/img/1.png" class="card-img-top" alt="Banco" />
     <div class="card-body">
-      <h5 class="card-title">Tarta de chocolate</h5> <span class="badge rounded-pill text-bg-info">Nuevo</span>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="detalleproducto.html" class="btn btn-primary">Ver mas</a>
+      <h5 class="card-title">RollingBank</h5>
+      <p class="card-text">Primer trabajo realizado durante el cursado para la practica del uso de Media Query, agregando un carrousel, cards e iconos mediante bootstrap.</p>
+      <a href="https://rollingbank-e.netlify.app/" target="_blank" className="btn btn-primary">Ver mas</a>
     </div>
   </div>
   
 </aside>
 <aside class="col-12 col-md-4 col-lg-3">
   <div class="card">
-    <img src="" class="card-img-top" alt="Medialuna" />
+    <img src="https://rollingstreaming.netlify.app/img/index-estreno-3.jpg" class="card-img-top" alt="Cine" />
     <div class="card-body">
-      <h5 class="card-title">Medialuna</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <button class="btn btn-primary disabled" type="button">Ver mas</button> 
-    </div>
-  </div>
-  
-</aside>
-<aside class="col-12 col-md-4 col-lg-3">
-  <div class="card">
-    <img src="" class="card-img-top" alt="Medialuna" />
-    <div class="card-body">
-      <h5 class="card-title">Medialuna</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <button class="btn btn-primary" type="button">Ver mas</button> 
+      <h5 class="card-title">RollingStreaming</h5>
+      <p class="card-text">Primer trabajo de fin de modulo formando una practica en grupo donde teniamos que implementar todo lo aprendido durante los primeros meses de clases html, bootstrap, media query, iconos, videos etc.</p>
+      <a href="https://rollingstreaming.netlify.app/" target="_blank" className="btn btn-primary">Ver mas</a> 
     </div>
   </div>
   
@@ -130,6 +116,17 @@ const Trabajos = () => {
       <h5 class="card-title">Medialuna</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <button class="btn btn-primary" type="button">Ver mas</button> 
+    </div>
+  </div>
+  
+</aside>
+<aside class="col-12 col-md-4 col-lg-3">
+  <div class="card">
+    <img src="https://tofuu.getjusto.com/orioneat-prod-resized/wjnWRkQBMbEfL5xLd-x-1800.webp" class="card-img-top" alt="Restaurante" />
+    <div class="card-body">
+      <h5 class="card-title">Comiteando-Restaurante</h5>
+      <p class="card-text">Ultimo trabajo de fin de modulo realizado durante el cursado en grupo implementando el uso de React, Git, ReactBootstrap complementando con las peticiones http put, get, delete.</p>
+      <a href="https://comitiando.netlify.app/" target="_blank" className="btn btn-primary">Ver mas</a> 
     </div>
   </div>
   

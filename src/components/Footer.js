03936de76581ae9c84ffdae3d3e40332debe6cec
¨Tomas <tomasgabriel1999@hotmail.com>
2023-02-25 11:05:29 -0300

    agregamos una ventana modal a nuestro footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { Container, Row, Col } from "react-bootstrap";
+import CursoR from "../assets/img/curso.png.png";
 
 const Footer = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Container>
       <Row className="my-5 justify-content-center">
@@ -16,15 +23,33 @@ const Footer = () => {
         <Col sm={12} md={5} lg={4}>
           <aside className="text-center mt-3">
             <h3>Curso certificado por:</h3>
+            <Button variant="primary" onClick={handleShow}>
+        Rolling Code School
+      </Button>
 
-            <li>Rolling Code School</li>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body> <img src={CursoR} className="diploma" /></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+            
+
+           
           </aside>
         </Col>
         <Col sm={12} md={5} lg={4}>
           <aside className="text-center mt-3">
             <h3>Contacto</h3>
-            <p>E-mail: tomasgabriel1999@hotmail.com</p>
-            <p>Tel/Wsp: 3816090335</p>
+
+            <p>
+              <i class="bi bi-envelope-at"></i>tomasgabriel1999@hotmail.com
+            </p>
+            <p>
+              <i class="bi bi-whatsapp"></i>3816090335
+            </p>
           </aside>
         </Col>
       </Row>
